85c32764bcb52b686a2a457715f4a55ec1c62312
Tavian Taylor <taviantaylor00@gmail.com>
2021-12-24 20:20:28 +0000

    Changed implementation to MVC

---
+++ api/controllers.py
@@ -0,0 +1,44 @@
+from django.http.response import HttpResponse
+from django.shortcuts import render
+from django.http import JsonResponse
+from django.contrib.auth import authenticate, login, logout
+from rest_framework import serializers
+from rest_framework import status
+
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .serializers import UserSerializer
+from .models import User,FridgeContent,Item
+
+class UserController():
+    def getAllUsers():
+        users = User.objects.all().exclude(is_superuser=True)
+        serializer = UserSerializer(users, many=True)
+        return Response(serializer.data)
+    def singleUserCheck(pk):
+        # Check to see if user exists
+        try:
+            user = User.objects.get(id=pk)
+        except User.DoesNotExist:
+            return HttpResponse(status=status.HTTP_404_NOT_FOUND) # Return 404
+    
+        # Check to see if user is admin i.e. superuser
+        if user.is_superuser == True:
+            return HttpResponse(status=status.HTTP_403_FORBIDDEN) # Return 403
+        return None
+    def getSingleUser(pk):
+        user = User.objects.get(id=pk)
+        serializer = UserSerializer(user, many=False)
+        return Response(serializer.data)
+    def updateSingleUser(request,pk):
+        serializer = UserSerializer(instance=user, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class FridgeContentController():
+    def getAllFridgeContent():
+        fridge_contents = FridgeContent.objects.all()
+        serializer = FridgeContentSerializer(fridge_contents, many=True)
+        return Response(serializer.data)
--- api/views.py
+++ api/views.py
@@ -8,7 +8,8 @@ from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .serializers import UserSerializer
-from .models import User
+from .models import User,FridgeContent,Item
+from .controllers import UserController, FridgeContentController
 
 '''Auth function'''
 @api_view(['POST'])
@@ -33,30 +34,22 @@ def logout(request):
 #Get all users who are not admins
 @api_view(['GET'])
 def allUsers(request):
-    users = User.objects.all().exclude(is_superuser=True)
-    serializer = UserSerializer(users, many=True)
-    return Response(serializer.data)
+    return UserController.getAllUsers()
 
 @api_view(['GET', 'PUT'])
 def singleUser(request, pk):
-    # Check to see if user exists
-    try:
-        user = User.objects.get(id=pk)
-    except User.DoesNotExist:
-        return HttpResponse(status=status.HTTP_404_NOT_FOUND) # Return 404
-    
-    # Check to see if user is admin i.e. superuser
-    if user.is_superuser == True:
-        return HttpResponse(status=status.HTTP_403_FORBIDDEN) # Return 403
+    error = UserController.singleUserCheck(pk)
+    if error is None:
+        # Return user model
+        if request.method == 'GET':
+            return UserController.getSingleUser(pk)
+        # Update included fields
+        if request.method == 'PUT':
+            return ?UserController.updateSingleUser(request,pk)
+    return error
 
-    # Return user model
-    if request.method == 'GET':
-        serializer = UserSerializer(user, many=False)
-        return Response(serializer.data)
-    # Update included fields
-    if request.method == 'PUT':
-        serializer = UserSerializer(instance=user, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+'''Fridge Content functions'''
+
+@api_view(['GET'])
+def allFridgeContent(request):
+    return FridgeContentController.getAllFridgeContent()
