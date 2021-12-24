92901c6a6cfd118ba1a508edcb5cb4cc68dd8777
Tavian Taylor <taviantaylor00@gmail.com>
2021-12-24 14:38:29 +0000

    Added serializers for FridgeContent and Item models

--- api/serializers.py
+++ api/serializers.py
@@ -1,9 +1,19 @@
 from django.db.models import fields
 from rest_framework import serializers
-from .models import User
+from .models import User, Item, FridgeContent
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['id', 'email', 'name', 'role', 'fridge_access']
         read_only_fields = ['id', 'email', 'name']
+
+class ItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Item
+        fields = '__all__'
+
+class FridgeContentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = FridgeContent
+        fields = '__all__'
