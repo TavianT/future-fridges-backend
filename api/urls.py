51e1692b12df6d3632cc2a9c5582d4184f01172a
Tavian Taylor <taviantaylor00@gmail.com>
2022-01-03 22:34:06 +0000

    added paths created in views to urls

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,16 @@
 from . import views
 from django.urls import path
 urlpatterns = [
+    #User paths
     path('users/', views.allUsers, name="users"),
     path('user/<str:pk>/', views.singleUser, name="user"),
+    #Auth paths
     path('login/', views.login, name='login'),
     path('logout/', views.logout, name='logout'),
+    #Item paths
+    path('item/<str:pk>/', views.singleItem, name='item'),
+    path('item-barcode', views.singleItemFromBarcode, name='item_from_barcode'),
+    #Fridge content paths
+    path('fridge-contents', views.allFridgeContent, name='fridge_contents'),
+    path('fridge-content', views.singleFridgeContent, name='fridge_content'),
 ]
